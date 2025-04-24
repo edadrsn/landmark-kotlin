f5030a64d2cf85a3f02ff69486c01135d0f386a6
Eda Dursun <edadrsn929@gmail.com>
2025-04-24 22:14:57 +0300

    Landmark class created and all datas added an arraylist

---
+++ app/src/main/java/com/example/landmarkkotlin/Landmark.kt
@@ -0,0 +1,5 @@
+package com.example.landmarkkotlin
+
+class Landmark(val name:String,val country:String,val image:Int){
+
+}
--- app/src/main/java/com/example/landmarkkotlin/MainActivity.kt
+++ app/src/main/java/com/example/landmarkkotlin/MainActivity.kt
@@ -9,13 +9,44 @@ import com.example.landmarkkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var landmarkList:ArrayList<Landmark>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Landmark sınıfı oluşturarak özellikleri kolay bir şekilde almayı sağladık
+        val pisa = Landmark("PISA", "ITALY", R.drawable.pisa);
+        val eiffel = Landmark("EIFFEL TOWER", "FRANCE", R.drawable.eiffel);
+        val colosseum = Landmark("COLOSSEUM", "ITALY", R.drawable.colosseum);
+        val big_ben = Landmark("BIG BEN", "ENGLAND", R.drawable.big_ben_tower);
+        val london_bridge = Landmark("LONDON BRIDGE", "ENGLAND", R.drawable.london_bridge);
+        val taj_mahal = Landmark("TAJ MAHAL", "INDIA", R.drawable.taj_mahal);
+        val peri_bacalari = Landmark("PERI BACALARI", "TURKEY", R.drawable.peri_bacalari);
+        val chandeokgung = Landmark("CHANDEOKGUNG", "KOREA", R.drawable.changdeokgung);
+        val la_sagra_da_familia = Landmark("LA SAGRA DA\n\t\tFAMILIA", "SPAIN", R.drawable.la_sagrada_familia);
+        val louvre_museum = Landmark("LOUVRE MUSEUM", "FRANCE", R.drawable.louvre_museum);
+        val aya_sofia = Landmark("AYA SOFYA", "TURKEY", R.drawable.ayasofia);
+        val cinque_terre = Landmark("CINQUE TERRE", "ITALY", R.drawable.cinque_terre);
+
+        //ArrayList ile tüm verileri tek bir lite içinde topladık
+        landmarkList=ArrayList<Landmark>()
+        landmarkList.add(pisa)
+        landmarkList.add(eiffel)
+        landmarkList.add(colosseum)
+        landmarkList.add(big_ben)
+        landmarkList.add(london_bridge)
+        landmarkList.add(taj_mahal)
+        landmarkList.add(peri_bacalari)
+        landmarkList.add(chandeokgung)
+        landmarkList.add(la_sagra_da_familia)
+        landmarkList.add(louvre_museum)
+        landmarkList.add(aya_sofia)
+        landmarkList.add(cinque_terre)
+
 
     }
 }
